860e4808464758263710b3d769380f64b45dace5
md-sanim-mia <hasansanim562@gmail.com>
2025-09-29 00:31:59 +0600

    error slove

--- src/app/modules/assessments/scenario.services.ts
+++ src/app/modules/assessments/scenario.services.ts
@@ -75,7 +75,7 @@ const updateScenario = async (
     data,
   });
 const scenario_id=result.id
-   const apiUrl = `http://206.162.244.131:8003/speech/generate-from-scenario?scenario_id=${scenario_id}`;
+   const apiUrl = `${config.ai_base_url}/speech/generate-from-scenario?scenario_id=${scenario_id}`;
   const response = await axios.post(apiUrl,{
     headers: { "Content-Type": "application/json" },
   });
