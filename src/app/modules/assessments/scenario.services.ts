29773f30637ef1ba7927a16f7a36847e89cba011
md-sanim-mia <hasansanim562@gmail.com>
2025-09-28 22:24:04 +0600

    some chenge

--- src/app/modules/assessments/scenario.services.ts
+++ src/app/modules/assessments/scenario.services.ts
@@ -74,11 +74,12 @@ const updateScenario = async (
     where: { id },
     data,
   });
-
-  const apiUrl = `${config.ai_base_url}/speech/generate-from-scenario/${result.id}`;
+const scenario_id=result.id
+   const apiUrl = `http://206.162.244.131:8003/speech/generate-from-scenario?scenario_id=${scenario_id}`;
   const response = await axios.post(apiUrl,{
     headers: { "Content-Type": "application/json" },
   });
+
   return result;
 };
 
